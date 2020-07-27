51c1a3f126fe5bf8f4efa273aa613fe34ddd45a9
jeongbeen <kkaatteelyn@gmail.com>
2020-07-27 19:36:28 +0900

    edit style

---
+++ app/src/main/java/com/example/idealperfume/Activity/PI_SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.idealperfume.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.idealperfume.R;
+
+public class PI_SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pi_search);
+    }
+}

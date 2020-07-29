915f5169b5c7dac0df65cd96dd1b682562a4ae02
cjyeon <cwd6372@gmail.com>
2020-07-29 20:04:59 +0900

    내픽이상향 및 이벤트 화면 생성2

---
+++ app/src/main/java/com/example/idealperfume/Adapter/MyPickAdapter.java
@@ -0,0 +1,44 @@
+package com.example.idealperfume.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.idealperfume.Fragment.MyPickFragment1;
+import com.example.idealperfume.Fragment.MyPickFragment2;
+
+import java.util.ArrayList;
+
+public class MyPickAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> items;
+    private ArrayList<String> menus = new ArrayList<String>();;
+
+    public MyPickAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        items = new ArrayList<>();
+        items.add(new MyPickFragment1());
+        items.add(new MyPickFragment2());
+
+        menus.add("제품별");
+        menus.add("브랜드별");
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return menus.get(position);
+    }
+}

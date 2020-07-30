b62d00537ea655cbf80381d58608041d47899298
jiwon <prismle73@gmail.com>
2020-07-30 15:43:01 +0900

    test

--- app/src/main/java/com/example/idealperfume/Activity/MainActivity.java
+++ app/src/main/java/com/example/idealperfume/Activity/MainActivity.java
@@ -3,16 +3,40 @@ package com.example.idealperfume.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.idealperfume.R;
 
 public class MainActivity extends AppCompatActivity{
+
+    int pressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if ( pressedTime == 0 ) {
+            Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다." , Toast.LENGTH_LONG).show();
+            pressedTime = (int) System.currentTimeMillis();
+        }
+        else {
+            int seconds = (int) (System.currentTimeMillis() - pressedTime);
+
+            if ( seconds > 2000 ) {
+                Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다." , Toast.LENGTH_LONG).show();
+                pressedTime = 0 ;
+            }
+            else {
+                super.onBackPressed();
+//                finish(); // app 종료 시키기
+            }
+        }
     }
 
 }

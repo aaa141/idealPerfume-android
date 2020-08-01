c5451c8e16751dcfffffa5894349b6a44c3fd246
jiwon <prismle73@gmail.com>
2020-08-01 21:26:19 +0900

    Merge branch 'master' of D:\AndroidP\idealPerfume-android with conflicts.

---
+++ app/src/main/java/com/example/idealperfume/Activity/QuickSearchActivity.java
@@ -0,0 +1,90 @@
+package com.example.idealperfume.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.example.idealperfume.R;
+import com.jaygoo.widget.OnRangeChangedListener;
+import com.jaygoo.widget.RangeSeekBar;
+
+public class QuickSearchActivity extends AppCompatActivity {
+    TextView tv_age;
+    SeekBar seekBar_age;
+    com.jaygoo.widget.RangeSeekBar rangeSeekBar;
+
+    int padding, sPos, xPos;
+    int lineCount=0; // 선택된 게열 수 카운팅
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_search);
+
+        tv_age = findViewById(R.id.tv_age);
+        seekBar_age = findViewById(R.id.seekBar_age);
+        rangeSeekBar = findViewById(R.id.rangeSeekbar);
+
+
+        rangeSeekBar.setIndicatorTextDecimalFormat("#,###");
+        rangeSeekBar.setIndicatorTextStringFormat("%s원");
+        rangeSeekBar.setProgress(0, rangeSeekBar.getMaxProgress());
+
+        tv_age.setX(155); // ****************************************
+
+        seekBar_age.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress==0) {
+                    seekBar.setProgress(1);
+                }else {
+                    getSeekbarxPos(seekBar, progress);
+                    tv_age.setX(xPos);
+                    tv_age.setText(progress + "0대");
+                    Log.d("progress", seekBar.getLeft() + "");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+        rangeSeekBar.setOnRangeChangedListener(new OnRangeChangedListener() {
+            @Override
+            public void onRangeChanged(RangeSeekBar view, float leftValue, float rightValue, boolean isFromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(RangeSeekBar view, boolean isLeft) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(RangeSeekBar view, boolean isLeft) {
+
+            }
+        });
+
+    }
+
+    private int getSeekbarxPos(SeekBar seekBar, int progress){
+        padding = seekBar.getPaddingLeft() + seekBar.getPaddingRight();
+        sPos = seekBar.getLeft() + seekBar.getPaddingLeft();
+        xPos = (seekBar.getWidth() - padding) * seekBar.getProgress() / seekBar.getMax() + sPos - (tv_age.getWidth() / 2);
+
+        return xPos;
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/idealperfume/Activity/ReviewRegActivity.java
@@ -0,0 +1,16 @@
+package com.example.idealperfume.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.idealperfume.R;
+
+public class ReviewRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_reg);
+    }
+}
